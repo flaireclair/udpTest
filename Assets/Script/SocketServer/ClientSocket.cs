bcdfbf01a03a3a6cf4ad5e6e63fd1ee22d50469d
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2019-08-24 15:34:55 +0900

    aaaaa

---
+++ Assets/Script/SocketServer/ClientSocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+
+public class ClientSocket : MonoBehaviour
+{
+  public string ipOrHost = "127.0.0.1";
+  public int port = 11888;
+    NetworkStream stream = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  private NetworkStream GetNetworkStream()
+  {
+    if (stream != null && stream.CanRead)
+    {
+      return stream;
+    }
+
+    TcpClient tcp = new TcpClient(ipOrHost, port);
+    Debug.Log("success conn server");
+
+    return tcp.GetStream();
+  }
+}
---
+++ Assets/Script/SocketServer/ServerTest.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Net.NetworkInformation;
+
+/*
+ * TestServer.cs
+ * SocketServerを継承、開くポートを指定して、送受信したメッセージを具体的に処理する
+ */
+namespace Script.SocketServer
+{
+	public class ServerTest : SocketServer {
+#pragma warning disable 0649
+		// ポート指定（他で使用していないもの、使用されていたら手元の環境によって変更）
+		[SerializeField]private int _port;
+#pragma warning restore 0649
+		
+		private void Start(){
+      // 接続中のIPアドレスを取得
+      var ipAddress = "0.0.0.0";
+      foreach (var ni in NetworkInterface.GetAllNetworkInterfaces())
+      {
+        if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+            ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+        {
+          foreach (var ip in ni.GetIPProperties().UnicastAddresses)
+          {
+            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+              ipAddress = ip.Address.ToString();
+              Debug.Log(ipAddress);
+            }
+          }
+        }
+      }
+      // 指定したポートを開く
+      Listen(ipAddress, _port);
+
+      // システムに接続情報をセット（表示用）
+      TCPSystem.Instance.SetIpAddressPort (ipAddress + ":" + _port);
+      Debug.Log(ipAddress + ":" + _port);
+		}
+
+		// クライアントからメッセージ受信
+		protected override void OnMessage(string msg){
+			base.OnMessage(msg);
+			
+			// ------------------------------------------------
+			// あとは送られてきたメッセージによって何かしたいことを書く
+			// ------------------------------------------------
+
+			// 今回は受信した整数値を表示用システムにセットする
+			int num;
+			// 整数値以外は何もしない
+			if (int.TryParse (msg, out num)) {
+				// ビュアーに値をセットする
+				MyViewer.Instance.SetNum (num);
+				// クライアントに受領メッセージを返す
+				Send (null);
+			} else {
+				// クライアントにエラーメッセージを返す
+				Send (null);
+			}
+		}
+
+	}
+}
